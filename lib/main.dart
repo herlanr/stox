6ad49d3ad0700c209646ee8747000c307ded1183
Herlan Ribeiro Rocha <herlan.ribeiro-rocha@capgemini.com>
2026-05-27 22:26:07 +0200

    horizontal blocked

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      builder: (context, child) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            if (orientation == Orientation.landscape) {
+              return const Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.screen_rotation, size: 50, color: Colors.grey),
+                      SizedBox(height: 20),
+                      Text(
+                        'Please rotate your device back to portrait mode.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+            return child!;
+          },
+        );
+      },
       home: HomeNavigation(),
     );
   }
 }
-
